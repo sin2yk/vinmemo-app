9b7253703842d5bc64b01d84b4999e3fcfa3d148
sin2yk <shinji.yoshikawa@gmail.com>
2025-12-08 16:26:06 +0900

    responsive

--- bottle_new.php
+++ bottle_new.php
@@ -237,6 +237,7 @@ if (!$event_id) {
 
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>VinMemo - ボトル登録</title>
     <link rel="stylesheet" href="style.css">
 </head>
--- events_new.php
+++ events_new.php
@@ -78,6 +78,7 @@ $page_title = 'VinMemo - New Event';
 
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?= htmlspecialchars($page_title) ?></title>
     <link rel="stylesheet" href="style.css">
 </head>
